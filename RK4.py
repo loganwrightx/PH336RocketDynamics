6f5813c6757995d101bbfc8eb265021edf0ab315
wesley-is-me <102831329+wesley-is-me@users.noreply.github.com>
2025-03-11 14:54:40 -0600

    RK4 from 295

--- RK4.py
+++ RK4.py
@@ -1,2 +1,14 @@
-def RK4():
-  
+def RK4(f, r, t, tf, h, thetalist, tlist):
+    while t<tf:
+        k1 = h*f(r,t)
+        k2 = h*f(r+.5*k1, t+.5*h)
+        k3 = h*f(r+.5*k2, t+.5*h)
+        k4 = h*f(r+k3, t+h)
+
+        #estimate new r
+        r += (k1+ 2*k2 + 2*k3 +k4)/6
+
+        # add new values to the lists for plotting
+        thetalist.append(r[0])
+        t += h
+        tlist.append(t)  
