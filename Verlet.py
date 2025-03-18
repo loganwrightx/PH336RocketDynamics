584c3efda436d845ce9d3ead88a0cef26ab71c82
wesley-is-me <102831329+wesley-is-me@users.noreply.github.com>
2025-03-18 08:38:52 -0600

    go away

--- Verlet.py
+++ Verlet.py
@@ -1,12 +1,5 @@
 from numpy import ndarray
 
-def velocity_verlet(x, v, dt, accel):
-    a = accel(x)
-    x_new = x + v*dt + 0.5*a*dt*dt
-    a_new = accel(x_new)
-    v_new = v + 0.5*(a + a_new)*dt
-    return x_new, v_new
-
 def Verlet(f: function, r: ndarray, t: float, dt: float) -> ndarray:
   r_half = f(r, t)
   r_full = f(r + r_half * dt / 2, t + dt / 2)
