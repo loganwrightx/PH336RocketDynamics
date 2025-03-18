25992be794cc5ac3af667b82495d6f5af0e3d531
Logan Wright <loganwrightx@icloud.com>
2025-03-18 09:01:11 -0600

    Made some fixes to the LaunchLoop function

---
+++ Differentials.py
@@ -0,0 +1,4 @@
+from numpy import ndarray
+
+def f(r: ndarray, t: float) -> ndarray:
+  return r
--- LaunchLoop.py
+++ LaunchLoop.py
@@ -1,30 +1,42 @@
-
-
 from Differentials import f
-from RK4 import rk4
-from numpy import array, transpose
+from RK4 import RK4
+from numpy import array, ndarray, sin, cos
+import matplotlib.pyplot as plt
 
+plt.style.use("dark_background")
 
-def loop(θ, dt, x0, y0, vx0, vy0):
+R, R_DOT, X, Y = 0, 1, 0, 1
+
+def loop(θ: float, v: float, dt: float, r0: ndarray, plot: bool = False):
     t = 0
+    v0 = array([v0 * sin(θ), v0 * cos(θ)], dtype=float)
+    r = array([r0, v0], dtype=float)
     
-    #
-    t_list = [0]
-    r_list = [[x0, y0]]
-    
+    t_list = [t]
+    x_list = [r[R, X]]
+    y_list = [r[R, Y]]
 
-    while y >= 0:
-        if t < 0.1 and ay < 0:
-            ay = 0 #Keep the rocket from going down when gravity is initially greater than thust
-
-        dr = rk4(f, t, dt)
-        r_list.append([r[0,0], r[0,1]])
-
-        r += dr 
-        t += dt #dt defined in rk4
+    while r[R, Y] >= 0:
+        dr = RK4(f, t, dt)
         
-        #Transposes r_list so that it's easy to find the maximum y value
-        max_height = max(transpose(array(r_list))[1])
+        if t < 0.1 and dr[R_DOT, Y] < 0:
+            dr[R_DOT, Y] = 0 # Keep the rocket from going down when gravity is initially greater than thust
+
+        r += dr
+        t += dt # dt defined in main
+        
+        x_list.append(r[R, X])
+        y_list.append(r[R, Y])
+        t_list.append(t)
+        
+        # transposes r_list so that it's easy to find the maximum y value
+    
+    max_height = max(y_list)
+    
+    if plot:
+        plt.plot(t_list, x_list)
+        plt.plot(t_list, y_list)
+        plt.show()
 
     return t, max_height
 
