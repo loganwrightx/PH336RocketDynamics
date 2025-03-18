531ce80e1db364f6bea5a0b9a508628c43f0f74d
Logan Wright <loganwrightx@icloud.com>
2025-03-18 08:36:02 -0600

    Added Verlet function

--- Verlet.py
+++ Verlet.py
@@ -1,3 +1,4 @@
+from numpy import ndarray
 # verlet seems pretty cool, I don't remember learning about it
 # Is this somewhat the idea?
 
@@ -7,3 +8,8 @@ def velocity_verlet(x, v, dt, accel):
     a_new = accel(x_new)
     v_new = v + 0.5*(a + a_new)*dt
     return x_new, v_new
+
+def Verlet(f: function, r: ndarray, t: float, dt: float, response: float = None) -> ndarray:
+  r_half = f(r, t, response)
+  r_full = f(r + r_half * dt / 2, t + dt / 2, response)
+  return dt * r_full
