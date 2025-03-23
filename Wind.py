86301feb5987b1a580cd50b71a7965396d7a5d72
Logan Wright <loganwrightx@icloud.com>
2025-03-23 00:17:45 -0600

    Exploring ways to incorporate smooth wind gusts and differentiable wind conditions for our simulation envirnonment. Set the framework for the Wind class to produce environment conditions, but needs work.

--- Wind.py
+++ Wind.py
@@ -2,3 +2,37 @@ from __future__ import annotations
 from typing import List
 from numpy import ndarray, array, float64, exp
 from numpy.random import normal as random_normal
+
+class Wind:
+  wind_direction: ndarray
+  direction_var: float
+  ambient_speed: float
+  gust_speed: float
+  speed_var: float
+  gust_frequency: float
+  frequency_var: float
+  
+  def __init__(self,
+    wind_direction: ndarray,
+    direction_var: float,
+    ambient_speed: float,
+    gust_speed: float,
+    speed_var: float,
+    gust_frequency: float,
+    frequency_var: float
+  ):
+    self.wind_direction = wind_direction
+    self.direction_var = direction_var
+    self.ambient_speed = ambient_speed
+    self.gust_speed = gust_speed
+    self.speed_var = speed_var
+    self.gust_frequency = gust_frequency
+    self.frequency_var = frequency_var
+    
+
+class Gust:
+  def __init__(self, t0: float):
+    self.t0 = t0
+  
+  def __getitem__(self, i: int) -> tuple[float, bool]:
+    return 
